d7a6f28c14afe077066da40e46877f7de8a5ded1
Hirusha Adikari <zesta5j7k@gmail.com>
2021-09-13 08:59:09 +0530

    more sounds

--- GUI/Rick and Morty Theme/Multi Threading/Sounds/file.py
+++ GUI/Rick and Morty Theme/Multi Threading/Sounds/file.py
@@ -2,11 +2,11 @@ import os
 import base64 as bb
 os.listdir()
 
-with open("im_rick_from_c137_no_harm_peace.mp3", "rb") as f1:
+with open("whatsup_glip_glops.mp3", "rb") as f1:
     f1c = f1.read()
     f1cbb = bb.b64encode(f1c)
 
-with open("im_rick_from_c137_no_harm_peace.txt", "wb") as f2:
+with open("whatsup_glip_glops.txt", "wb") as f2:
     f2.write(f1cbb)
 
 with open("test.mp3", "wb") as f3:
