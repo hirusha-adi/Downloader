285285f240805d641063006aaa1e1c10a0116e78
Hirusha Adikari <zesta5j7k@gmail.com>
2021-09-17 18:34:14 +0530

    v0.8 - fixed a huge bug with strings (file name)

---
+++ GUI/v0.8/start.py
@@ -0,0 +1,13 @@
+# #########################
+# Made by ZeaCeR#5641
+# a.k.a Hirusha Adikari
+# For any issue, please contact me!
+# #########################
+
+import homewindow, images
+
+try:
+    homewindow.window.mainloop()
+finally:
+    images.DELETE_ALL()
+
